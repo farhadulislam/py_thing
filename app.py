58ac30400f8feb0e8a2bb71750c11e0c3e89679d
Farhd Islam <farhad.islam@outlook.com>
2022-05-30 20:52:22 +0600

    Added venv related files in pycharm

---
+++ app.py
@@ -0,0 +1,3 @@
+
+
+print("Demo Python app")
--- simple.py
+++ simple.py
@@ -2,3 +2,17 @@ def doSth(apple):
     print(apple)
 
 doSth("apple")
+
+def showMyName():
+    print("Farhad")
+
+showMyName()
+
+likes = input()
+dislikes = input()
+views = input()
+rank = (((likes-dislikes)/views)*100)*(((likes-dislikes)/(likes+dislikes))*100)
+print(rank)
+
+
+
