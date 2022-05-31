b8e7456692ba48c27fb072b4740b3f9a2ee182af
Farhd Islam <farhad.islam@outlook.com>
2022-05-31 17:42:06 +0600

    Added tuple demo

---
+++ demo_tuple.py
@@ -0,0 +1,26 @@
+#TUPLE DEMO
+#TUPLE allows duplicates
+#TUPLE is unodered
+#TUPLE items can be of any types
+
+tuple_1 = ('Chittagong', 'Dhaka', 'Munshiganj', 'Sirajganj', 'Comilla', 'Feni', 'Lalmonirhat')
+
+print(tuple_1)
+
+#INSERT into a tuple. Convert tuple into list, appned an item, and then finally revert it to tuple
+#This is beacuse tuple is IMMUTABLE
+
+list1_from_tuple_1 = list(tuple_1)
+list1_from_tuple_1.append('Rajshahi')
+print(list1_from_tuple_1)
+tuple_1= tuple(list1_from_tuple_1)
+print(tuple_1)
+
+#ONE ITEM TUPLE, ensure a comma is placed
+tuple_2 =('One',)
+print(type(tuple_2))
+
+# Below isn't a TUPLE
+tuple_3 =('One')
+print(type(tuple_3))
+
