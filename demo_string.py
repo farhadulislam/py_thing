8deb2eef25749e6c60db0ba745929a2f606a93d5
Farhd Islam <farhad.islam@outlook.com>
2022-06-03 16:02:10 +0600

    Added more string methods

--- demo_string.py
+++ demo_string.py
@@ -3,11 +3,37 @@ sentence = "This is a sentence"
 
 print('This is \'nt a joke')
 
-age = 36
+title = "This IS A Title"
+print(title.istitle())
+
+age = str(36)
+print(age.isdecimal)
 
 print(sentence.istitle())
 
 uni = 'university'
 print(uni.upper())
+
 uni_upper = uni.upper()
+print(f"{uni_upper } is in uppercase : \t")
 print(uni_upper.isupper())
+
+uni_lower = uni_upper.lower()
+print(f"{uni_lower} is lower : ")
+print(uni_lower.islower())
+print(uni_lower.islower())
+
+alphanumeric = "ABCH123"
+print(f"{alphanumeric} is alphanumric")
+print(alphanumeric.isalpha())
+
+
+hello_world = "Hello, world!"
+
+print(hello_world.startswith("Hello"))
+print(hello_world.startswith("World"))
+
+
+print(' '.isspace())
+
+#The join() method is called on a string, gets passed a list of strings, and returns a string.
---
+++ demo_string_02.py
@@ -0,0 +1,7 @@
+animals_joined = ', '.join(['cats', 'rats', 'bats'])
+print(type(animals_joined))
+
+print(animals_joined.split())
+
+hello_world = "Hello, world"
+print(hello_world.partition('o'))
