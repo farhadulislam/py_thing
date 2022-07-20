9285e9449a9802c8e8f6c4c9c024b353ab64c6f6
Farhd Islam <farhad.islam@outlook.com>
2022-07-21 02:14:02 +0600

    added type hinting - adds code readibility

---
+++ type_hinting.py
@@ -0,0 +1,15 @@
+# compatible only with py version 3.6 and above
+
+def isElligible(platform: str, age: int) -> bool:
+    if platform == "facebook" and age > 13:
+        return True
+    elif platform == "twitter" and age > 18:
+        return False
+
+
+def isElligible2(platform: str, age: int) -> bool:
+    return (platform == "facebook" and age > 13) or (platform == "twitter" and age > 18)
+
+
+print(isElligible("facebook", 18))
+print(isElligible2("twitter", 20))
