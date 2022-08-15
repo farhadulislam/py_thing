9c32b3da85031ec5e96bad21276f3c5e0ca963c6
Farhd Islam <farhad.islam@outlook.com>
2022-08-15 22:40:56 +0600

    added tkinter demo

---
+++ GUI_STUFF/demo_gui_01.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+window = Tk()
+
+window.geometry("420x420")
+window.title("Farhad Islam")
+icon = PhotoImage(file='icon.png')
+window.iconphoto(True,icon)
+window.config(background ="green")
+window.mainloop()
